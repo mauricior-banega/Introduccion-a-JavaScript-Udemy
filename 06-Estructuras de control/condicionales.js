a2cb43977744165bbcbd60618d229730625946ff
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-21 16:39:28 -0300

    04-01
    Estructuras de control en JS- Condicionales (IF)

---
+++ 06-Estructuras de control/condicionales.js
@@ -0,0 +1,56 @@
+var mascotas = "perro";
+
+//Condicional IF
+
+/*
+if(mascotas =="perro")
+{
+    document.write("guaw!"); //Instruccion que escribe el HTML
+}
+else if (mascotas =="gato")
+{
+    document.write("miaw!");
+}
+else
+{
+    document.write("No conozco a tu mascota");
+}
+*/
+
+//--------------------
+
+/*
+var edad = 18;
+
+if (edad >=18)
+{
+    document.write("Bienvenido a nuestro bar");
+}
+else
+{
+    document.write("No puedes entrar");
+}
+ */
+
+        //Actividades
+
+var puntaje = 100;
+
+if (puntaje>=100)
+{
+    document.write("Tu puntaje es igual o mayor a 100");
+}
+else if (puntaje<100)
+{
+    document.write("Tu puntaje es inferior a 100");
+}
+else if (puntaje<=50)
+{
+    document.write("Tu puntaje mayor o igual a 50 pero menor que 100");
+}
+else
+{
+    document.write("Tu puntaje es inferior a 50");
+}
+
+

583581c95ae408e2e92265a2d7e9fcb485cbc5d7
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-21 17:24:54 -0300

    04-02
    Bucles

---
+++ 06-Estructuras de control/buclesfor.js
@@ -0,0 +1,37 @@
+
+var clase = ["Juan","Pedro","Manolo"];
+
+/*Comento para mostrar que recorra el array por su longitud
+for (var conteo =0; conteo < 10; conteo++)
+{
+//  document.write(conteo + "<br>");
+    //Imprimirá los nº del 0 al 9
+
+    document.write(clase[0] + "<br>");
+    //Imprimirá los 10 veces Juan
+}
+
+*/
+
+for (var conteo =0; conteo < clase.length; conteo++)
+{
+  //  document.write(clase[0] + "<br>");
+    //Muestra 3 veces Juan, longitud del array 3
+
+   // document.write(clase[conteo] + "<br>");
+    //Muestra los 3 valores del array clase, dado que el array empieza en 0 y termina en 2, el for tambien lo hace. De esa forma recorremos mostrando cada valor del arreglo en toda su longitud.
+}
+
+
+        //Actividades
+
+var naturaleza = ["roca","tierra","agua","aire","fuego","hielo","electricidad","hielo","vegetacion","animales"];
+
+for (var conteo =0; conteo < naturaleza.length; conteo++)
+{
+
+    document.write(naturaleza[conteo] + "<br>");
+  
+}
+
+

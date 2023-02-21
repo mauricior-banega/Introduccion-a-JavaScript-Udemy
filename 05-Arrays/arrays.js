c1d96e070947ec60f0dc213f4bc6fd527c78a089
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-21 15:54:43 -0300

    03-01
    Métodos de arreglos. Ejemplos aplicados.

---
+++ 05-Arrays/arrays.js
@@ -0,0 +1,17 @@
+var frutas = ["mangos", "piñas", "platanos", "peras"];
+
+//console.log (frutas); Aperecen todos los elementos del array
+
+console.log (frutas[0]); //Muestra el 1º elemento del array
+
+console.log(frutas.length); //Muestra la cantidad de elementos que tiene el arreglo
+
+
+    //Actividades
+var semana =["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
+
+console.log(semana[2]);
+
+console.log(semana.length);
+
+

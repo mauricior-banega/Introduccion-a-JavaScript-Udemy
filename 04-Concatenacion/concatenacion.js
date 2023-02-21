8fc59c77bf308856099aa184eb183d21a30735aa
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-21 15:45:45 -0300

    02-04
    Concatenación en JS

---
+++ 04-Concatenacion/concatenacion.js
@@ -0,0 +1,36 @@
+var calculo = "bella" + "vista";
+
+//alert(calculo); //Muestra: bellavista
+
+var grupo = "juan"; 
+//+= para esta instancia no iría porque no se puede agregar valor a una variable que hasta ese punto no la tiene. Pero para los demás si.
+
+    grupo +=", jose";
+
+    grupo +=", diego";
+
+    grupo +=", manolo";
+
+//alert(grupo);
+
+
+    //Actividades
+
+
+var texto1 = "hoja" + "lata";
+var texto2 = "agri" + "dulce";
+var texto3 = "alti" + "bajo";
+var texto4 = "rompe" + "cabezas";
+
+//Muestro por consola que es mas rapido de ver
+console.log(texto1);
+console.log(texto2);
+console.log(texto3);
+console.log(texto4);
+
+var nombrecompleto="mauricio";
+nombrecompleto+=" rubén";
+nombrecompleto+=" banega";
+
+console.log(nombrecompleto);
+

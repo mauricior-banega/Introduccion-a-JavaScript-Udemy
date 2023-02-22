ef390ed07f64e0209a023d65b1e59b279f7cc447
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-22 16:23:57 -0300

    08-03
    Modificando documentos: Selección de elementos.

---
+++ 10-Modificar documentos/selectores.js
@@ -0,0 +1,12 @@
+
+//Accedemos al ID HTML y pedimos que nos recupere el className, que le asignamos y es (tituloPrincipal). Lo veremos a traves de un alert.
+
+alert (document.getElementById("titular").className);
+
+//Accedemos al ID HTML y pedimos que nos recupere el contenido de ese párrafo. Lo veremos tambien a traves de un alert.
+
+alert (document.getElementById("dato").innerHTML);
+
+
+
+

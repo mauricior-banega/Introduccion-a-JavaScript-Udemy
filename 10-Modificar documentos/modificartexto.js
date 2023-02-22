7513ba852f0534c2eed575ce6005ac4df8a58ae0
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-22 16:24:49 -0300

    08-02
    Modificando documentos: Modificando texto usando DOM.

---
+++ 10-Modificar documentos/modificartexto.js
@@ -0,0 +1,13 @@
+
+function cambiarTexto()
+{
+    document.getElementById("dato").innerHTML="<strong>Soy</strong> el nuevo texto";
+    //No queremos saber que contenido tiene (innerHTML) sino cambiar el contenido del texto. Asique igualamos y le damos el nuevo valor.
+}
+
+
+function cambiarTexto2()
+{
+    document.getElementById("dato").innerHTML="Estoy realizando la actividad de la clase, creando un nuevo texto aplicando innetHTML y seleccionando el ID 'dato'.";
+    //No queremos saber que contenido tiene (innerHTML) sino cambiar el contenido del texto. Asique igualamos y le damos el nuevo valor.
+}

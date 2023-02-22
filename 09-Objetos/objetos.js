a8865149d360fe908246c6ca183d6f8c7ddd9831
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-22 00:24:59 -0300

    07-01
    Objetos: Crear y usar objetos

---
+++ 09-Objetos/objetos.js
@@ -0,0 +1,22 @@
+
+//Objeto auto
+var auto = {}
+
+auto.color = "Rojo";
+auto.dueno = "Manolo";
+auto.marca = "Ferrari";
+
+//document.write("Mi auto es de color "+ auto.color);
+
+
+        //Actividades
+
+var casa = {};
+
+casa.color = "gris con blanco";
+casa.direccion = "Antonio Donovan";
+casa.dueno = "Carlos Ruben Banega";
+casa.ventanas = "6";
+
+document.write("Descripcion de mi casa: "+"Mi casa es de color "+casa.color+", cuya direccion es: "+casa.direccion+". La casa pertenece a: "+casa.dueno+" y posee "+casa.ventanas+" ventanas.");
+
